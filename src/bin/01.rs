a81eaf2aa3144fce76d29e345cb19b46b415c113
Devon Morris <devonmorris1992@gmail.com>
2022-12-01 22:23:59 -0500

    part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,12 +1,18 @@
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn calculate_calories_per_elf(input: &str) -> Option<Vec<u32>> {
     let chunks : Vec<&str> = input.split("\n\n").collect();
     let calories_per_elf_per_item : Vec<Vec<u32>> = chunks.iter().map(|s| -> Vec<u32> { s.lines().map(|l| l.parse().unwrap()).collect() }).collect();
-    let calories_per_elf : Vec<u32> = calories_per_elf_per_item.iter().map(|v| v.iter().sum()).collect();
+    Some(calories_per_elf_per_item.iter().map(|v| v.iter().sum()).collect())
+}
+pub fn part_one(input: &str) -> Option<u32> {
+    let calories_per_elf : Vec<u32> = calculate_calories_per_elf(input).unwrap();
     Some(calories_per_elf.iter().max().unwrap().to_owned())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut calories_per_elf : Vec<u32> = calculate_calories_per_elf(input).unwrap();
+    calories_per_elf.sort();
+    calories_per_elf.reverse();
+    Some(calories_per_elf[0..3].iter().sum())
 }
 
 fn main() {
@@ -22,7 +28,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(69795));
     }
 
     #[test]
