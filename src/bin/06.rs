0882d72e84d7099a2aa3fb0e4a590f7990865bcf
Devon Morris <devonmorris1992@gmail.com>
2022-12-08 14:44:54 -0500

    Day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,9 +1,22 @@
-pub fn part_one(input: &str) -> Option<u32> {
+use std::collections::HashSet;
+
+pub fn find_unique_idx(input: &str, win_size : usize) -> Option<u32> {
+    for (i, window) in input.as_bytes().windows(win_size).enumerate() {
+        let win_set : HashSet<u8> = HashSet::from_iter(window.iter().cloned());
+        if win_set.len() == win_size {
+            let idx : u32 = (i + win_size).try_into().ok()?;
+            return Some(idx);
+        }
+    }
     None
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    find_unique_idx(input, 4)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    find_unique_idx(input, 14)
 }
 
 fn main() {
@@ -19,7 +32,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 6);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(7));
     }
 
     #[test]
